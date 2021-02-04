748e69e1d47594eeee75b273f70f710eb5aadab3
Ibekason Alexander <alibekason@gmail.com>
2021-02-04 18:45:42 +0100

    Removed Creds from github

---
+++ src/main/java/com/misprojects/robertssupermarket/controller/StoreRestController.java
@@ -0,0 +1,9 @@
+package com.misprojects.robertssupermarket.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StoreRestController {
+
+
+}
