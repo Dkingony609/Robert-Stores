32887e28da991599ea99f1a7c8f321499ed7b196
Ibekason Alexander <alibekason@gmail.com>
2021-02-04 15:06:55 +0100

    Added Web Design to Server

---
+++ src/main/java/com/misprojects/robertssupermarket/RobertssupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.misprojects.robertssupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobertssupermarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobertssupermarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/misprojects/robertssupermarket/RobertssupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misprojects.robertssupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobertssupermarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
