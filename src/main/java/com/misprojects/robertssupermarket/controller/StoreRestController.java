a6f2ced76f49d2a91c5e26d26770775667b0752c
Ibekason Alexander <alibekason@gmail.com>
2021-02-04 21:10:30 +0100

    Added Rest Api for app

--- src/main/java/com/misprojects/robertssupermarket/controller/StoreRestController.java
+++ src/main/java/com/misprojects/robertssupermarket/controller/StoreRestController.java
@@ -1,9 +1,57 @@
 package com.misprojects.robertssupermarket.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.misprojects.robertssupermarket.model.PaymentMethods;
+import com.misprojects.robertssupermarket.model.Sales;
+import com.misprojects.robertssupermarket.repository.StoreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("api/v1/sales")
 public class StoreRestController {
+
+    private final StoreRepository storeRepository;
+
+    @GetMapping("")
+    public List<Sales> getAllSales(){
+        return storeRepository.findAll();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteSales(@PathVariable("id") long id){
+        storeRepository.findById(id).ifPresent(storeRepository::delete);
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("{id}")
+    public Sales getSaleByID(@PathVariable long id){
+        return storeRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
+    @PatchMapping("{id}")
+    public Sales updateSales(@PathVariable("id") long id, @RequestBody SalesInfoDto salesInfoDto){
+        Sales sales = storeRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        sales.setAmount(salesInfoDto.getAmount())
+        .setPaymentMethod(PaymentMethods.valueOf(salesInfoDto.getPaymentMethod().toUpperCase()))
+        .setCustomerName(salesInfoDto.getCustomerName())
+        .setPurchases(Stream.of(salesInfoDto.getPurchases().split(",")).collect(Collectors.toList()));
+        return storeRepository.save(sales);
+
+    }
+
+    @GetMapping("staff")
+    public List<Sales> getSalesByStaff(@RequestParam("name") String name){
+        return storeRepository.findByStaff(name.toLowerCase());
+    }
+
 
 
 }
--- src/main/java/com/misprojects/robertssupermarket/repository/StoreRepository.java
+++ src/main/java/com/misprojects/robertssupermarket/repository/StoreRepository.java
@@ -3,5 +3,8 @@ package com.misprojects.robertssupermarket.repository;
 import com.misprojects.robertssupermarket.model.Sales;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StoreRepository  extends JpaRepository<Sales, Long> {
+    List<Sales> findByStaff(String staff);
 }
